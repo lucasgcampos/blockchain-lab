d2aa97cf93f3a6aa956767ff0904638e7b89e739
Lucas Campos <lucasgcampos.contato@gmail.com>
2024-03-06 01:19:48 -0300

    Add transfers function to token

--- erc-20/src/XDevToken.sol
+++ erc-20/src/XDevToken.sol
@@ -7,13 +7,52 @@ contract XDevToken {
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    mapping(address => uint256) balanceOf;
+    event Transfer(address indexed _from, address indexed  _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     constructor() {
         name = "XDev Token";
         symbol = "XDEV";
         decimals = 8;
-        totalSupply = 100000000;
+        totalSupply = 1000000 * 10e8;
+        balanceOf[msg.sender] = 1000000 * 10e8;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require (balanceOf[msg.sender] >= _value, "INSUFFICIENT_AMOUNT");
+        
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(allowance[_from][msg.sender] >= _value, "INSUFFICIENT_ALLOWANCE");
+
+
+        require([msg.sender] >= _value, "INSUFFICIENT_AMOUNT");
+
+        balanceOf[_from] -= _value;
+        allowance[_from][msg.sender] -= _value;
+        balanceOf[msg.sender] += _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(allowance[msg.sender] >= _value, "INSUFFICIENT_AMOUNT");
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
     }
 }
